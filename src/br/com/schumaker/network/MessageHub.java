d52efb16cb0a1707bc7081d787bc63674acb83a6
hudson <hudson@192.168.0.1>
2014-12-31 00:18:49 -0200

    #Limpeza de System.out....

--- src/br/com/schumaker/network/MessageHub.java
+++ src/br/com/schumaker/network/MessageHub.java
@@ -17,16 +17,16 @@ public class MessageHub {
         try {
             server = new ServerSocket(HsCommons.RXPORT);
             run = true;
-            System.out.println("> MesssageHub service running...");
-            System.out.println("> MesssageHub listening on port: " + HsCommons.RXPORT);
+            System.out.println("MesssageHub service running...");
+            System.out.println("MesssageHub listening on port: " + HsCommons.RXPORT);
         } catch (Exception e) {
             System.err.println("MessageHub:\n" + e);
         }
         while (run) {
             try {
                 incoming = server.accept();
-                System.out.println("Client IPx: " + incoming.getInetAddress());
-                System.out.println("Client Port: " + incoming.getPort());
+//                System.out.println("Client IPx: " + incoming.getInetAddress());
+//                System.out.println("Client Port: " + incoming.getPort());
                 new MessageHubSession(incoming).start();
             } catch (Exception e) {
                 System.err.println("MessageHub2:\n" + e);
